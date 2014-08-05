7e06b1d92b924870c16545cc1961ded79abe01bb
Ben Longbons <b.r.longbons@gmail.com>
2014-08-04 18:46:27 -0700

    Try again

--- install.conf.sh
+++ install.conf.sh
@@ -8,9 +8,14 @@
 # 1. we really do need a full install.
 # 2. this script is only run if it's not already installed
 
+test -z "${PREFIX}" || echo "Custom PREFIX=${PREFIX}"
+test -z "${SUDO}" || echo "Custom SUDO=${SUDO}"
+test -z "${LDCONFIG}" || echo "Custom LDCONFIG=${LDCONFIG}"
+test -z "${MAKE}" || echo "Custom MAKE=${MAKE}"
+
 prefix=${PREFIX:-/usr/local}
 
-if install -d ${prefix} 2>/dev/null
+if install -d ${prefix} 2>/dev/null && test -w ${prefix}
 then
     sudo=
     ldconfig=:
@@ -26,6 +31,8 @@ make=${MAKE:-make}
 
 unset PREFIX SUDO LDCONFIG MAKE
 
+# This script can only set paths for building the C libraries themselves.
+# You'll need to export the config yourself in order to build the Rust lib.
 if test -z "${PKG_CONFIG_PATH}"
 then
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${prefix}/lib/pkgconfig
